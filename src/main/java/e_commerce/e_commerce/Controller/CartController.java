91b8cc6040b83ac9a129ac899bb8c8feb3e7422c
Ayush Kumar <ayushraj315@gmail.com>
2025-01-02 19:30:48 +0530

    Cart Section Updated

--- src/main/java/e_commerce/e_commerce/Controller/CartController.java
+++ src/main/java/e_commerce/e_commerce/Controller/CartController.java
@@ -7,7 +7,6 @@ import e_commerce.e_commerce.Service.CartService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
