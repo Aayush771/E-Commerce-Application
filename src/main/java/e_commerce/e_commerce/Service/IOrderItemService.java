e856d17f712e34fc37b83e9cfaf085d063da5508
Ayush Kumar <ayushraj315@gmail.com>
2025-01-05 23:54:49 +0530

    Order Seller DashBord Created

--- src/main/java/e_commerce/e_commerce/Service/IOrderItemService.java
+++ src/main/java/e_commerce/e_commerce/Service/IOrderItemService.java
@@ -1,5 +1,16 @@
 package e_commerce.e_commerce.Service;
 
+import e_commerce.e_commerce.Entity.OrderItem;
+
 public interface IOrderItemService {
-    
+     OrderItem savOrderItem(OrderItem orderItem);
+
+     OrderItem updateOrderItem(OrderItem orderItem);
+
+     void deleteOrderItem(Long orderItemId);    
+
+     OrderItem getOrderItem(Long orderItemId);
+
+     Iterable<OrderItem> getAllOrderItems();
+
 }
--- src/main/java/e_commerce/e_commerce/Service/OrderItemService.java
+++ src/main/java/e_commerce/e_commerce/Service/OrderItemService.java
@@ -1,5 +1,9 @@
 package e_commerce.e_commerce.Service;
 
+import e_commerce.e_commerce.Entity.OrderItem;
+
 public class OrderItemService {
-    
+   private OrderItem savOrderItem(final OrderItem orderItem) {
+       return null;
+   }
 }
