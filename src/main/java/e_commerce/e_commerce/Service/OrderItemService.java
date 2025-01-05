510de73bd215eff21822e8b806eda6c417e84143
Ayush Kumar <ayushraj315@gmail.com>
2025-01-05 23:56:39 +0530

    Order Section Updated

--- src/main/java/e_commerce/e_commerce/Service/OrderItemService.java
+++ src/main/java/e_commerce/e_commerce/Service/OrderItemService.java
@@ -2,8 +2,35 @@ package e_commerce.e_commerce.Service;
 
 import e_commerce.e_commerce.Entity.OrderItem;
 
-public class OrderItemService {
-   private OrderItem savOrderItem(final OrderItem orderItem) {
+public class OrderItemService implements IOrderItemService{
+    @Override
+   public OrderItem savOrderItem(final OrderItem orderItem) {
        return null;
    }
+
+
+
+@Override
+public OrderItem updateOrderItem(OrderItem orderItem) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateOrderItem'");
+}
+
+@Override
+public void deleteOrderItem(Long orderItemId) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteOrderItem'");
+}
+
+@Override
+public OrderItem getOrderItem(Long orderItemId) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getOrderItem'");
+}
+
+@Override
+public Iterable<OrderItem> getAllOrderItems() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getAllOrderItems'");
+}
 }
